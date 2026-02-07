d8166c6a4920ece6bc78c2385515fb27486af326
Simon Kucik <simon.kucik@studenti.unitn.it>
2026-02-07 15:29:09 +0100

    Add users, chats, and notifications routes

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import groups from './groups.js';
 import meetings from './meetings.js';
-
+import users from './users.js'; 
+import chats from './chats.js';
+import notifications from './notifications.js';
 
 const app = express();
 
@@ -12,5 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 // resource routing
 app.use('/api/v1/groups', groups);
 app.use('/api/v1/groups/:group_id/meetings', meetings);
+app.use('/api/v1/users', users);
+app.use('/api/v1/chats', chats);
+app.use('/api/v1/notifications', notifications);
 
 export default app;
