9f06c2e793660ff239b6ae24060e16e93a3a2a3f
Samuele Gobbi <samuelegobbi2005@gmail.com>
2026-02-19 01:32:04 +0100

    Piccolo cambiamento grafico

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -43,7 +43,7 @@ watch(route, () => {
 
       <div class="flex items-center gap-4">
         
-        <RouterLink to="/createGroup" class="btn btn-sm btn-ghost gap-2 hidden sm:flex">
+        <RouterLink to="/createGroup" class="btn btn-sm btn-ghost gap-2 hidden sm:flex bg-green-400 hover:bg-green-500">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
           </svg>
